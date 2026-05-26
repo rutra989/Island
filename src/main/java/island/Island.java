a39a26aaeb2ce27b807e950d00a588e1e759cbc9
Denis <rutra989@mail.ru>
2026-05-26 17:37:56 +0300

    refactor: update Parameters fields and Island constructor

--- src/main/java/island/Island.java
+++ src/main/java/island/Island.java
@@ -4,11 +4,11 @@ import servise.Parametrs;
 
 public class Island {
     private Location [][] locations;
-
+        // создание объекта остров с инициализацией размера массива
     public Island(Parametrs parametrs) {
-        this.locations = new Location[parametrs.heightSize][parametrs.lengthSize];
+        this.locations = new Location[parametrs.getHeightSize()][parametrs.getLengthSize()];
     }
-
+    // метод заполняет массив локациями - пока без заселения))
     public void init(){
         for (int i = 0; i < locations.length; i++) {
             for (int j = 0; j < locations[i].length; j++) {
--- src/main/java/servise/Parametrs.java
+++ src/main/java/servise/Parametrs.java
@@ -1,6 +1,17 @@
 package servise;
 
+import lombok.Getter;
+import lombok.Setter;
+import organism.Animal_Type;
+
+import java.util.Map;
+@Getter
+@Setter
 public class Parametrs {
-    public int heightSize;
-    public int lengthSize;
+    private int heightSize; // ширина массива
+    private int lengthSize; //длина массива
+    private int tickDuration; // длительность такта симуляции
+    private boolean stopCondition; // условие остановки симуляции
+    private Map<Animal_Type, Integer> initialCount; // начальное кол-во животных;
+    private Map<Animal_Type,Integer> numberCubs; // количество детенышей
 }
