ebf8221aa8b5a1fa9c65a8aa21a3bf14fa3db136
Denis <rutra989@mail.ru>
2026-05-26 11:43:05 +0300

    feat: add AnimalType enum and update Plant constants

---
+++ src/main/java/Organism/Animal_Type.java
@@ -0,0 +1,39 @@
+package Organism;
+
+import lombok.Getter;
+
+@Getter
+public enum Animal_Type {
+
+
+    BEAR(500, 5, 2, 80),
+    BOA_SNAKE(15, 30, 1, 3),
+    EAGLE(6, 20, 3, 1),
+    FOX(8, 30, 2, 2),
+    WOLF(50, 30, 3, 8),
+    BUFFALO(700, 10, 3, 100),
+    DEER(300, 20, 4, 50),
+    DUCK(1, 200, 4, 0.15),
+    GOAT(60, 140, 3, 15),
+    HOG(400, 50, 2, 50),
+    HORSE(400, 20, 4, 60),
+    MOUSE(0.05, 500, 1, 0.01),
+    RABBIT(2, 150, 2, 0.45),
+    SHEEP(70, 140, 3, 15),
+    CATERPILLAR(0.01, 1000, 0, 0);
+
+
+    private final double maxWeight;
+    private final int maxCount;
+    private final int maxSpeed;
+    private final double maxFood;
+
+
+    Animal_Type(double maxWeight, int maxCount, int maxSpeed, double maxFood) {
+        this.maxWeight = maxWeight;
+        this.maxCount = maxCount;
+        this.maxSpeed = maxSpeed;
+        this.maxFood = maxFood;
+    }
+
+    }
--- src/main/java/Organism/Plants.java
+++ src/main/java/Organism/Plants.java
@@ -1,6 +1,12 @@
 package Organism;
 
+import lombok.Getter;
+
+@Getter
 public class Plants extends Organism {
+
+    private final double maxWeight = 1;
+    private final int maxCount = 200;
 
 
 }
