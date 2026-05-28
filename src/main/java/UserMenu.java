0d7ae46af39704893121950bfa68dab03c271912
Denis <rutra989@mail.ru>
2026-05-28 10:29:45 +0300

    feat: Add UserMenu class refactor: add method simulationFactory to MainMenu class

---
+++ src/main/java/UserMenu.java
@@ -0,0 +1,16 @@
+import servise.MainMenu;
+import servise.Parameters;
+import servise.Simulation;
+
+import java.util.Scanner;
+
+public class UserMenu {
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        MainMenu mainMenu = new MainMenu();
+        mainMenu.simulationFactory();
+        mainMenu.initParameters();
+    }
+}
--- src/main/java/servise/MainMenu.java
+++ src/main/java/servise/MainMenu.java
@@ -1,9 +1,21 @@
 package servise;
 
-public class MainMenu {
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
+public class MainMenu {
     Parameters parameters = new Parameters();
+
+
+    // установка параметров.
     public void initParameters(){
 
+    }
+    // запуск симуляции.
+    public void simulationFactory(){
+        Simulation simulation = new Simulation(parameters);
+        simulation.start();
     }
 }
